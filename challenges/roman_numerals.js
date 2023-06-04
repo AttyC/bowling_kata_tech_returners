f2cd3217507d79cd2e2d536e0c9147400076c1e6
Atty Cronin <antoniajcronin@gmail.com>
2023-06-04 18:33:08 +0100

    Test conversion to Roman Numerals using an array of objects

--- challenges/roman_numerals.js
+++ challenges/roman_numerals.js
@@ -1,4 +1,13 @@
+const romanNumerals = {
+  1: "I",
+  2: "II",
+  3: "III",
+  4: "IV",
+  5: "V",
+  10: "X",
+};
+
 export function convertNumberToRomanNumeral(number) {
   if (number === undefined) throw new Error("number is required");
-  return "I";
+  return romanNumerals[number];
 }
--- test/roman_numerals.test.js
+++ test/roman_numerals.test.js
@@ -4,4 +4,7 @@ describe("prints individual digits as string of Roman Numerals", () => {
   test("prints I if number is 1", () => {
     expect(convertNumberToRomanNumeral(1)).toBe("I");
   });
+  test("prints II if number is 2", () => {
+    expect(convertNumberToRomanNumeral(2)).toBe("II");
+  });
 });
