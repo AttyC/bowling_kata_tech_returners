2375d3ba2d70de96373238a41ef9c4855a92f2d8
Atty Cronin <antoniajcronin@gmail.com>
2023-06-04 19:32:19 +0100

    Refactor tests into more describe blocks and add more tests for numbers under 100

--- test/roman_numerals.test.js
+++ test/roman_numerals.test.js
@@ -1,6 +1,6 @@
 import { convertNumberToRomanNumeral } from "../challenges/roman_numerals";
 
-describe("prints individual digits as string of Roman Numerals", () => {
+describe("converts numbers under 10 to string of Roman Numerals", () => {
   test("prints I if number is 1", () => {
     expect(convertNumberToRomanNumeral(1)).toBe("I");
   });
@@ -11,6 +11,25 @@ describe("prints individual digits as string of Roman Numerals", () => {
     expect(convertNumberToRomanNumeral(10)).toBe("X");
   });
   test("prints XX if number is 20", () => {
+    expect(convertNumberToRomanNumeral(20)).toBe("XX");
+  });
+});
+describe("converts numbers divisible by 10 to string of Roman Numerals", () => {
+  test("prints X if number is 10", () => {
+    expect(convertNumberToRomanNumeral(10)).toBe("X");
+  });
+  test("prints XX if number is 20", () => {
+    expect(convertNumberToRomanNumeral(20)).toBe("XX");
+  });
+});
+describe("converts numbers over 10 and under 100 to string of Roman Numerals", () => {
+  test("prints XIV if number is 16", () => {
+    expect(convertNumberToRomanNumeral(16)).toBe("XVI");
+  });
+  test("prints II if number is 27", () => {
+    expect(convertNumberToRomanNumeral(2)).toBe("II");
+  });
+  test("prints XX if number is 98", () => {
     expect(convertNumberToRomanNumeral(20)).toBe("XX");
   });
   test("prints XIV if number is 16", () => {
